7427f3da0435666a02f00e89ce0fe842c6599e74
OlhaKlopotivska <olyakocherhina@gmail.com>
2023-06-15 20:24:45 +0200

    advantages commit

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -14,6 +14,34 @@ $(document).ready(function() {
 });
 
 
+mobileOnlySlider(".advantages-slider", true, false, 991);
+
+function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+    var slider = $($slidername);
+    var settings = {
+        mobileFirst: true,
+        dots: $dots,
+        arrows: $arrows,
+        responsive: [{
+            breakpoint: $breakpoint,
+            settings: "unslick"
+        }]
+    };
+
+    slider.slick(settings);
+
+    $(window).on("resize", function() {
+        if ($(window).width() > $breakpoint) {
+            return;
+        }
+        if (!slider.hasClass("slick-initialized")) {
+            return slider.slick(settings);
+        }
+    });
+} // Mobile Only Slider
+
+
+
 function checkboxContainerPadding() {
     const checkButton = document.querySelectorAll('.check-button');
     checkButton.forEach(item => {
@@ -26,3 +54,12 @@ if (document.querySelector('.calculation-form')) {
 } else {
     null
 }
+
+
+document.getElementById('fileInput').onchange = function() {
+    //short name
+    document.getElementById('file-name').innerHTML = this.files[0].name;
+
+    //long name
+    // document.getElementById('file-name').innerHTML = this.value;
+};
